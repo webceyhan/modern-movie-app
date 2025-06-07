caa2db38637362a08ec4fa8dff8ac3797897958b
Evren Ceyhan <evren.ceyhan@gmail.com>
2025-06-07 21:39:33 +0200

    adjust movie list title margin

--- src/App.tsx
+++ src/App.tsx
@@ -48,7 +48,7 @@ export default function App() {
         </header>
 
         <section className="all-movies">
-          <h2>All Movies</h2>
+          <h2 className="mt-[40px]">All Movies</h2>
 
           {isLoading && <Spinner />}
 
