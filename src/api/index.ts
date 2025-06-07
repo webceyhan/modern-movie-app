78e26b6314ff7ad6eb784e37af4141ed68a587c8
Evren Ceyhan <evren.ceyhan@gmail.com>
2025-06-07 21:34:00 +0200

    add movie api methods to fetch

---
+++ src/api/index.ts
@@ -0,0 +1,47 @@
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+const API_BASE_URL = "https://api.themoviedb.org/3";
+
+const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${API_KEY}`,
+  },
+};
+
+export type Movie = {
+  adult: boolean;
+  backdrop_path: string | null;
+  genre_ids: number[];
+  id: number;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string | null;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+};
+
+type Response = {
+  page: number;
+  results: Movie[];
+  total_pages: number;
+  total_results: number;
+};
+
+export const fetchMovies = async (): Promise<Response> => {
+  const url = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+
+  const response = await fetch(url, API_OPTIONS);
+
+  if (!response.ok) {
+    throw new Error(response.statusText);
+  }
+
+  return response.json();
+};
