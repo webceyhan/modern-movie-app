e55f1c5a1110df0926a2e74704280c12239771f3
Evren Ceyhan <evren.ceyhan@gmail.com>
2025-06-07 21:34:21 +0200

    add initial movie list fetching

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,34 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { fetchMovies, type Movie } from "./api";
 import Search from "./components/Search";
 
 export default function App() {
   //
   const [searchTerm, setSearchTerm] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [movieList, setMovieList] = useState<Movie[]>([]);
+
+  const loadMovies = async () => {
+    // Reset error message
+    setIsLoading(true);
+    setErrorMessage("");
+
+    try {
+      const data = await fetchMovies();
+      setMovieList(data.results);
+    } catch (error) {
+      setMovieList([]);
+      setErrorMessage("Failed to fetch movies. Please try again later.");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    // Load movies when the component mounts
+    loadMovies();
+  }, []);
 
   return (
     <main>
@@ -17,9 +42,25 @@ export default function App() {
             Find <span className="text-gradient">Movies</span> You'll Enjoy
             Without the Hassle
           </h1>
+
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
         </header>
 
-        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        <section className="all-movies">
+          <h2>All Movies</h2>
+
+          {isLoading && <p className="text-white">Loading...</p>}
+
+          {errorMessage && <p className="error">{errorMessage}</p>}
+
+          <ul className="text-white">
+            {movieList.map((movie) => (
+              <li key={movie.id}>
+                <h3>{movie.title}</h3>
+              </li>
+            ))}
+          </ul>
+        </section>
       </div>
     </main>
   );
